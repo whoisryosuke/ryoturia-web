80db499e7469a9cf07a4c89157efc176dc5610f7
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-10-29 15:09:08 -0700

    :lipstick: animated color

--- src/components/canvas/AudioViz/WaveformLine/WaveformLine.tsx
+++ src/components/canvas/AudioViz/WaveformLine/WaveformLine.tsx
@@ -4,6 +4,10 @@ import WaveformBox from "./WaveformBox";
 import { GroupProps, useFrame } from "@react-three/fiber";
 import mapRange from "@/helpers/mapRange";
 import { Mesh } from "three";
+import { lerp } from "three/src/math/MathUtils";
+
+// const WAVE_BASE_COLOR = [0.8, 0.8, 0.8];
+const WAVE_BASE_COLOR = [0, 0, 0];
 
 type Props = GroupProps & {};
 
@@ -39,7 +43,8 @@ const WaveformLine = (props: Props) => {
       const height = 2;
       const amplitude = 7;
       // -890 to -90
-      let binMapped = mapRange(levels[index], -100, 50, 0, 1) * amplitude;
+      let waveHeightNormalized = mapRange(levels[index], -100, 50, 0, 1);
+      let waveHeightAmplified = waveHeightNormalized * amplitude;
       // let binMapped = mapRange(levels[i], -890, -90, 0, 1);
       // const normalized = Math.max(binMapped, 0);
       // p.vertex(i * 12, binMapped - 500);
@@ -47,7 +52,24 @@ const WaveformLine = (props: Props) => {
       // const sin = Math.sin(i / 5 + delta / 1000) * 5;
 
       // boxRefs.current[i].position.y = sin + halfwayDownScreen - normalized;
-      boxRefs.current[i].position.y = binMapped;
+      boxRefs.current[i].position.y = waveHeightAmplified;
+
+      const adjustedWaveHeight = waveHeightNormalized * 10;
+      boxRefs.current[i].material.color.r = lerp(
+        WAVE_BASE_COLOR[0],
+        0,
+        adjustedWaveHeight
+      );
+      boxRefs.current[i].material.color.g = lerp(
+        WAVE_BASE_COLOR[1],
+        0,
+        adjustedWaveHeight
+      );
+      boxRefs.current[i].material.color.b = lerp(
+        WAVE_BASE_COLOR[2],
+        1,
+        adjustedWaveHeight
+      );
     }
   });
 
